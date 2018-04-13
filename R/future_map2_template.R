1d54af79b2a9557e097890b0e7ed4441e470b47b
DavisVaughan <mvaugh15@uncc.edu>
2018-04-13 10:37:55 -0400

    Initial commit

---
+++ R/future_map2_template.R
@@ -0,0 +1,350 @@
+future_map2_template <- function(.map, .type, .x, .y, .f, ..., future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+
+  # Create function from .f
+  .f <- purrr::as_mapper(.f, ...) # ... required in case you pass .null / .default through for purrr::as_mapper.numeric
+
+  objectSize <- import_future("objectSize")
+
+  stopifnot(is.function(.f))
+
+  stopifnot(is.logical(future.lazy))
+
+  stopifnot(!is.null(future.seed))
+
+  stopifnot(length(future.scheduling) == 1, !is.na(future.scheduling),
+            is.numeric(future.scheduling) || is.logical(future.scheduling))
+
+  ## Nothing to do?
+  n.x <- length(.x)
+  n.y <- length(.y)
+  if (n.x == 0 || n.y == 0)  {
+    # To stay consistent with purrr::map_*() return types
+    # purrr handles this at the C level
+    empty_type <- switch(
+      .type,
+      "character" = character(),
+      "double"    = double(),
+      "list"      = list(),
+      "integer"   = integer(),
+      "logical"   = logical()
+    )
+    return(empty_type)
+  }
+
+  ## Improper lengths
+  if (n.x != n.y && !(n.x == 1 || n.y == 1)) {
+    msg <- sprintf("`.x` (%i) and `.y` (%i) are different lengths", n.x, n.y)
+    stop(msg, call. = FALSE)
+  }
+
+  ## Recycle .x or .y to correct length if needed
+  # At this point, the only allowed extension is if .x or .y is length 1
+  if(n.x > n.y) .y <- rep(.y, times = n.x)
+  if(n.y > n.x) .x <- rep(.x, times = n.y)
+
+  debug <- getOption("future.debug", FALSE)
+
+  if (debug) mdebug("future_lapply() ...")
+
+  ## NOTE TO SELF: We'd ideally have a 'future.envir' argument also for
+  ## future_lapply(), cf. future().  However, it's not yet clear to me how
+  ## to do this, because we need to have globalsOf() to search for globals
+  ## from the current environment in order to identify the globals of
+  ## arguments 'FUN' and '...'. /HB 2017-03-10
+  future.envir <- environment()  ## Not used; just to clarify the above.
+
+  envir <- future.envir
+
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## 1. Global variables
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## The default is to gather globals
+  if (is.null(future.globals)) future.globals <- TRUE
+
+  packages <- NULL
+  globals <- future.globals
+  if (is.logical(globals)) {
+    ## Gather all globals?
+    if (globals) {
+      if (debug) mdebug("Finding globals ...")
+
+      expr <- do.call(call, args = c(list(".f"), list(...)))
+      gp <- getGlobalsAndPackages(expr, envir = envir, globals = TRUE)
+      globals <- gp$globals
+      packages <- gp$packages
+      gp <- NULL
+
+      if (debug) {
+        mdebug(" - globals found: [%d] %s", length(globals), hpaste(sQuote(names(globals))))
+        mdebug(" - needed namespaces: [%d] %s", length(packages), hpaste(sQuote(packages)))
+        mdebug("Finding globals ... DONE")
+      }
+    } else {
+      ## globals = FALSE
+      globals <- c(".f", names(list(...)), "...")
+      globals <- globalsByName(globals, envir = envir, mustExist = FALSE)
+    }
+  } else if (is.character(globals)) {
+    globals <- unique(c(globals, ".f", names(list(...)), "..."))
+    globals <- globalsByName(globals, envir = envir, mustExist = FALSE)
+  } else if (is.list(globals)) {
+    names <- names(globals)
+    if (length(globals) > 0 && is.null(names)) {
+      stop("Invalid argument 'future.globals'. All globals must be named.")
+    }
+  } else {
+    stop("Invalid argument 'future.globals': ", mode(globals))
+  }
+  globals <- as.FutureGlobals(globals)
+  stopifnot(inherits(globals, "FutureGlobals"))
+
+  names <- names(globals)
+  if (!is.element(".f", names)) {
+    globals <- c(globals, .f = .f)
+  }
+
+  if (!is.element("...", names)) {
+    if (debug) mdebug("Getting '...' globals ...")
+    dotdotdot <- globalsByName("...", envir = envir, mustExist = TRUE)
+    dotdotdot <- as.FutureGlobals(dotdotdot)
+    dotdotdot <- resolve(dotdotdot)
+    attr(dotdotdot, "total_size") <- objectSize(dotdotdot)
+    if (debug) mdebug("Getting '...' globals ... DONE")
+    globals <- c(globals, dotdotdot)
+  }
+
+  ## Assert there are no reserved variables names among globals
+  reserved <- intersect(c("...future.f", "...future.x_ii", "...future.y_ii",
+                          "...future.seeds_ii"), names)
+  if (length(reserved) > 0) {
+    stop("Detected globals using reserved variables names: ",
+         paste(sQuote(reserved), collapse = ", "))
+  }
+
+  ## Avoid .f() clash with map(.x, .f, ...) below.
+  names <- names(globals)
+  names[names == ".f"] <- "...future.f"
+  names(globals) <- names
+
+  if (debug) {
+    mdebug("Globals to be used in all futures:")
+    mdebug(paste(capture.output(str(globals)), collapse = "\n"))
+  }
+
+
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## 2. Packages
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # purrr is always included
+  packages <- unique(c(packages, "purrr"))
+
+  if (!is.null(future.packages)) {
+    stopifnot(is.character(future.packages))
+    future.packages <- unique(future.packages)
+    stopifnot(!anyNA(future.packages), all(nzchar(future.packages)))
+    packages <- unique(c(packages, future.packages))
+  }
+
+  if (debug) {
+    mdebug("Packages to be attached in all futures:")
+    mdebug(paste(capture.output(str(packages)), collapse = "\n"))
+  }
+
+
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## 3. Reproducible RNG (for sequential and parallel processing)
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  seed <- future.seed
+
+  ## Placeholder for all RNG stream seeds.
+  seeds <- NULL
+
+  ## Don't use RNGs? (seed = FALSE)
+  if (is.logical(seed) && !is.na(seed) && !seed) seed <- NULL
+
+  # Use RNGs?
+  if (!is.null(seed)) {
+    if (debug) mdebug("Generating random seeds ...")
+
+    ## future_lapply() should return with the same RNG state regardless of
+    ## future strategy used. This is be done such that RNG kind is preserved
+    ## and the seed is "forwarded" one step from what it was when this
+    ## function was called. The forwarding is done by generating one random
+    ## number. Note that this approach is also independent on length(.x) and
+    ## the diffent FUN() calls.
+    oseed <- next_random_seed()
+    on.exit(set_random_seed(oseed))
+
+    ## A pregenerated sequence of random seeds?
+    if (is.list(seed)) {
+      if (debug) mdebug("Using a pre-define stream of random seeds ...", n.x)
+
+      nseed <- length(seed)
+      if (nseed != n.x) {
+        stop("Argument 'seed' is a list, which specifies the sequence of seeds to be used for each element in '.x', but length(seed) != length(.x): ", nseed, " != ", n.x)
+      }
+
+      ## Assert same type of RNG seeds?
+      ns <- unique(unlist(lapply(seed, FUN = length), use.names = FALSE))
+      if (length(ns) != 1) {
+        stop("The elements of the list specified in argument 'seed' are not all of the same lengths (did you really pass RNG seeds?): ", hpaste(ns))
+      }
+
+      ## Did use specify scalar integers as meant for set.seed()?
+      if (ns == 1L) {
+        stop("Argument 'seed' is invalid. Pre-generated random seeds must be valid .Random.seed seeds, which means they should be all integers and consists of two or more elements, not just one.")
+      }
+
+      types <- unlist(lapply(seed, FUN = typeof), use.names = FALSE)
+      if (!all(types == "integer")) {
+        stop("The elements of the list specified in argument 'seed' are not all integers (did you really pass RNG seeds?): ", hpaste(unique(types)))
+      }
+
+      ## Check if valid random seeds are specified.
+      ## For efficiency, only look at the first one.
+      if (!is_valid_random_seed(seed[[1]])) {
+        stop("The list in argument 'seed' does not seem to hold elements that are valid .Random.seed values: ", capture.output(str(seeds[[1]])))
+      }
+
+      seeds <- seed
+
+      if (debug) mdebug("Using a pre-define stream of random seeds ... DONE", n.x)
+    } else {
+      if (debug) mdebug("Generating random seed streams for %d elements ...", n.x)
+
+      ## Generate sequence of _all_ RNG seeds starting with an initial seed
+      ## '.seed' that is based on argument 'seed'.
+      .seed <- as_lecyer_cmrg_seed(seed)
+
+      seeds <- vector("list", length = n.x)
+      for (ii in seq_len(n.x)) {
+        ## RNG substream seed used in call FUN(.x[[ii]], ...):
+        ## This way each future can in turn generate further seeds, also
+        ## recursively, with minimal risk of generating the same seeds as
+        ## another future. This should make it safe to recursively call
+        ## future_lapply(). /HB 2017-01-11
+        seeds[[ii]] <- nextRNGSubStream(.seed)
+
+        ## Main random seed for next iteration (= ii + 1)
+        .seed <- nextRNGStream(.seed)
+      }
+
+      if (debug) mdebug("Generating random seed streams for %d elements ... DONE", n.x)
+    }
+
+    if (debug) mdebug("Generating random seeds ... DONE")
+  } ## if (!is.null(seed))
+
+
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## 4. Load balancing ("chunking")
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  if (is.logical(future.scheduling)) {
+    if (future.scheduling) {
+      nbr_of_futures <- nbrOfWorkers()
+      if (nbr_of_futures > n.x) nbr_of_futures <- n.x
+    } else {
+      nbr_of_futures <- n.x
+    }
+  } else {
+    ## Treat 'future.scheduling' as the number of futures per worker.
+    stopifnot(future.scheduling >= 0)
+    nbr_of_workers <- nbrOfWorkers()
+    if (nbr_of_workers > n.x) nbr_of_workers <- n.x
+    nbr_of_futures <- future.scheduling * nbr_of_workers
+    if (nbr_of_futures < 1) {
+      nbr_of_futures <- 1L
+    } else if (nbr_of_futures > n.x) {
+      nbr_of_futures <- n.x
+    }
+  }
+
+  chunks <- splitIndices(n.x, ncl = nbr_of_futures)
+  if (debug) mdebug("Number of chunks: %d", length(chunks))
+
+
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## 5. Create futures
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## Add argument placeholders
+  globals_extra <- as.FutureGlobals(list(...future.x_ii = NULL, ...future.y_ii = NULL, ...future.seeds_ii = NULL))
+  attr(globals_extra, "resolved") <- TRUE
+  attr(globals_extra, "total_size") <- 0
+  globals <- c(globals, globals_extra)
+
+  ## At this point a globals should be resolved and we should know their total size
+  ##  stopifnot(attr(globals, "resolved"), !is.na(attr(globals, "total_size")))
+
+  ## To please R CMD check
+  ...future.f <- ...future.x_ii <- ...future.y_ii <- ...future.seeds_ii <- NULL
+
+  nchunks <- length(chunks)
+  fs <- vector("list", length = nchunks)
+  if (debug) mdebug("Number of futures (= number of chunks): %d", nchunks)
+
+  if (debug) mdebug("Launching %d futures (chunks) ...", nchunks)
+  for (ii in seq_along(chunks)) {
+    chunk <- chunks[[ii]]
+    if (debug) mdebug("Chunk #%d of %d ...", ii, length(chunks))
+
+    ## Subsetting outside future is more efficient
+    globals_ii <- globals
+    globals_ii[["...future.x_ii"]] <- .x[chunk]
+    globals_ii[["...future.y_ii"]] <- .y[chunk]
+    ##    stopifnot(attr(globals_ii, "resolved"))
+
+    ## Using RNG seeds or not?
+    if (is.null(seeds)) {
+      if (debug) mdebug(" - seeds: <none>")
+      fs[[ii]] <- future({
+
+        .map(seq_along(...future.x_ii), .f = function(jj) {
+          ...future.x_jj <- ...future.x_ii[[jj]]
+          ...future.y_jj <- ...future.y_ii[[jj]]
+          ...future.f(...future.x_jj, ...future.y_jj, ...)
+        })
+
+      }, envir = envir, lazy = future.lazy, globals = globals_ii, packages = packages)
+    } else {
+      if (debug) mdebug(" - seeds: [%d] <seeds>", length(chunk))
+      globals_ii[["...future.seeds_ii"]] <- seeds[chunk]
+      fs[[ii]] <- future({
+
+        .map(seq_along(...future.x_ii), .f = function(jj) {
+          ...future.x_jj <- ...future.x_ii[[jj]]
+          ...future.y_jj <- ...future.y_ii[[jj]]
+          assign(".Random.seed", ...future.seeds_ii[[jj]], envir = globalenv(), inherits = FALSE)
+          ...future.f(...future.x_jj, ...future.y_jj, ...)
+        })
+
+      }, envir = envir, lazy = future.lazy, globals = globals_ii, packages = packages)
+    }
+
+    ## Not needed anymore
+    rm(list = c("chunk", "globals_ii"))
+
+    if (debug) mdebug("Chunk #%d of %d ... DONE", ii, nchunks)
+  } ## for (ii ...)
+  if (debug) mdebug("Launching %d futures (chunks) ... DONE", nchunks)
+
+  ## Not needed anymore
+  rm(list = c("chunks", "globals", "envir"))
+
+  ## 4. Resolving futures
+  if (debug) mdebug("Resolving %d futures (chunks) ...", nchunks)
+  values <- values(fs)
+  if (debug) mdebug("Resolving %d futures (chunks) ... DONE", nchunks)
+
+  ## Not needed anymore
+  rm(list = "fs")
+
+  if (debug) mdebug("Reducing values from %d chunks ...", nchunks)
+  values <- fold(values, c)
+  names(values) <- names(.x) # purrr does the same thing with map2
+  if (debug) mdebug("Reducing values from %d chunks ... DONE", nchunks)
+
+  if (debug) mdebug("future_lapply() ... DONE")
+
+  values
+}
---
+++ R/utils.R
@@ -0,0 +1,195 @@
+# ------------------------------------------------------------------------------
+# Copied from rlang
+
+is_true <- function(x) {
+  identical(x, TRUE)
+}
+
+is_installed <- function(pkg) {
+  is_true(requireNamespace(pkg, quietly = TRUE))
+}
+
+# ------------------------------------------------------------------------------
+# Unexported functions from purrr
+
+probe <- function(.x, .p, ...) {
+  if (is_logical(.p)) {
+    stopifnot(length(.p) == length(.x))
+    .p
+  }
+  else {
+    purrr::map_lgl(.x, .p, ...)
+  }
+}
+
+inv_which <- function(x, sel) {
+  if (is.character(sel)) {
+    names <- names(x)
+    if (is.null(names)) {
+      stop("character indexing requires a named object",
+           call. = FALSE)
+    }
+    names %in% sel
+  }
+  else if (is.numeric(sel)) {
+    seq_along(x) %in% sel
+  }
+  else {
+    stop("unrecognised index type", call. = FALSE)
+  }
+}
+
+# ------------------------------------------------------------------------------
+# util
+
+
+# ------------------------------------------------------------------------------
+# from future.apply
+
+## From R.utils 2.0.2 (2015-05-23)
+hpaste <- function(..., sep = "", collapse = ", ", lastCollapse = NULL, maxHead = if (missing(lastCollapse)) 3 else Inf, maxTail = if (is.finite(maxHead)) 1 else Inf, abbreviate = "...") {
+  if (is.null(lastCollapse)) lastCollapse <- collapse
+
+  # Build vector 'x'
+  x <- paste(..., sep = sep)
+  n <- length(x)
+
+  # Nothing todo?
+  if (n == 0) return(x)
+  if (is.null(collapse)) return(x)
+
+  # Abbreviate?
+  if (n > maxHead + maxTail + 1) {
+    head <- x[seq_len(maxHead)]
+    tail <- rev(rev(x)[seq_len(maxTail)])
+    x <- c(head, abbreviate, tail)
+    n <- length(x)
+  }
+
+  if (!is.null(collapse) && n > 1) {
+    if (lastCollapse == collapse) {
+      x <- paste(x, collapse = collapse)
+    } else {
+      xT <- paste(x[1:(n-1)], collapse = collapse)
+      x <- paste(xT, x[n], sep = lastCollapse)
+    }
+  }
+
+  x
+} # hpaste()
+
+mdebug <- function(...) {
+  if (!getOption("future.debug", FALSE)) return()
+  message(sprintf(...))
+} ## mdebug()
+
+## When 'default' is specified, this is 30x faster than
+## base::getOption().  The difference is that here we use
+## use names(.Options) whereas in 'base' names(options())
+## is used.
+getOption <- local({
+  go <- base::getOption
+  function(x, default = NULL) {
+    if (missing(default) || match(x, table = names(.Options), nomatch = 0L) > 0L) go(x) else default
+  }
+}) ## getOption()
+
+
+
+get_random_seed <- function() {
+  env <- globalenv()
+  env$.Random.seed
+}
+
+set_random_seed <- function(seed) {
+  env <- globalenv()
+  if (is.null(seed)) {
+    rm(list = ".Random.seed", envir = env, inherits = FALSE)
+  } else {
+    env$.Random.seed <- seed
+  }
+}
+
+next_random_seed <- function(seed = get_random_seed()) {
+  sample.int(n = 1L, size = 1L, replace = FALSE)
+  seed_next <- get_random_seed()
+  stopifnot(!any(seed_next != seed))
+  invisible(seed_next)
+}
+
+is_valid_random_seed <- function(seed) {
+  oseed <- get_random_seed()
+  on.exit(set_random_seed(oseed))
+  env <- globalenv()
+  env$.Random.seed <- seed
+  res <- tryCatch({
+    sample.int(n = 1L, size = 1L, replace = FALSE)
+  }, simpleWarning = function(w) w)
+  !inherits(res, "simpleWarning")
+}
+
+is_lecyer_cmrg_seed <- function(seed) {
+  is.numeric(seed) && length(seed) == 7L &&
+    all(is.finite(seed)) && seed[1] == 407L
+}
+
+# @importFrom utils capture.output
+as_lecyer_cmrg_seed <- function(seed) {
+  ## Generate a L'Ecuyer-CMRG seed (existing or random)?
+  if (is.logical(seed)) {
+    stopifnot(length(seed) == 1L)
+    if (!is.na(seed) && !seed) {
+      stop("Argument 'seed' must be TRUE if logical: ", seed)
+    }
+
+    oseed <- get_random_seed()
+
+    ## Already a L'Ecuyer-CMRG seed?  Then use that as is.
+    if (!is.na(seed) && seed) {
+      if (is_lecyer_cmrg_seed(oseed)) return(oseed)
+    }
+
+    ## Otherwise, generate a random one.
+    on.exit(set_random_seed(oseed), add = TRUE)
+    RNGkind("L'Ecuyer-CMRG")
+    return(get_random_seed())
+  }
+
+  stopifnot(is.numeric(seed), all(is.finite(seed)))
+  seed <- as.integer(seed)
+
+  ## Already a L'Ecuyer-CMRG seed?
+  if (length(seed) == 7L) {
+    if (seed[1] != 407L) {
+      stop("Argument 'seed' must be L'Ecuyer-CMRG RNG seed as returned by parallel::nextRNGStream() or an single integer: ", capture.output(str(seed)))
+    }
+    return(seed)
+  }
+
+  ## Generate a new L'Ecuyer-CMRG seed?
+  if (length(seed) == 1L) {
+    oseed <- get_random_seed()
+    on.exit(set_random_seed(oseed), add = TRUE)
+    RNGkind("L'Ecuyer-CMRG")
+    set.seed(seed)
+    return(get_random_seed())
+  }
+
+  stop("Argument 'seed' must be of length 1 or 7 (= 1+6):", capture.output(str(seed)))
+}
+
+
+import_from <- function(name, default = NULL, package) {
+  ns <- getNamespace(package)
+  if (exists(name, mode = "function", envir = ns, inherits = FALSE)) {
+    get(name, mode = "function", envir = ns, inherits = FALSE)
+  } else if (!is.null(default)) {
+    default
+  } else {
+    stop(sprintf("No such '%s' function: %s()", package, name))
+  }
+}
+
+import_future <- function(name, default = NULL) {
+  import_from(name, default = default, package = "future")
+}
