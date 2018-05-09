bdadd5823c3b519a53665bbd1ff6a4018272a6e2
DavisVaughan <mvaugh15@uncc.edu>
2018-05-09 11:04:50 -0400

    Complete support for future_invoke_map and friends

--- R/future_invoke_map.R
+++ R/future_invoke_map.R
@@ -7,6 +7,22 @@
 #' @inheritParams future_map
 #' @inheritParams purrr::invoke_map
 #'
+#' @examples
+#'
+#' plan(multiprocess)
+#'
+#' df <- dplyr::tibble(
+#'   f = c("runif", "rpois", "rnorm"),
+#'   params = list(
+#'     list(n = 10),
+#'     list(n = 5, lambda = 10),
+#'     list(n = 10, mean = -3, sd = 10)
+#'   )
+#' )
+#'
+#' future_invoke_map(df$f, df$params)
+#'
+#'
 #' @export
 #'
 future_invoke_map <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
@@ -17,8 +33,48 @@ future_invoke_map <- function(.f, .x = list(NULL), ..., .env = NULL, .progress =
 
 #' @rdname future_invoke_map
 #' @export
+future_invoke_map_chr <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  future_map2_chr(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
+}
+
+#' @rdname future_invoke_map
+#' @export
+future_invoke_map_dbl <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  future_map2_dbl(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
+}
+
+#' @rdname future_invoke_map
+#' @export
+future_invoke_map_int <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  future_map2_int(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
+}
+
+#' @rdname future_invoke_map
+#' @export
+future_invoke_map_lgl <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  future_map2_lgl(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
+}
+
+#' @rdname future_invoke_map
+#' @export
 future_invoke_map_dfr <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   future_map2_dfr(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
+}
+
+#' @rdname future_invoke_map
+#' @export
+future_invoke_map_dfc <- function(.f, .x = list(NULL), ..., .env = NULL, .progress = FALSE, future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  future_map2_dfc(.f, .x, purrr::invoke, ..., .env = .env, .progress = .progress, future.globals = future.globals, future.packages = future.packages, future.seed = future.seed, future.lazy = future.lazy, future.scheduling = future.scheduling)
 }
