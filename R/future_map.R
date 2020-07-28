664e8a8d3a89a8dd521718fb2a77ef000a26781d
DavisVaughan <davis@rstudio.com>
2020-07-28 13:56:49 -0400

    Document `.p` and `.at` in `future_map_if()` and `future_map_at()`

--- R/future_map.R
+++ R/future_map.R
@@ -118,6 +118,7 @@ future_map_dfc <- function(.x, .f, ..., .progress = FALSE, .options = future_opt
 #' @rdname future_map
 #' @export
 #' @importFrom purrr list_along set_names
+#' @inheritParams purrr::map_if
 future_map_if <- function(.x, .p, .f, ..., .progress = FALSE, .options = future_options()) {
   sel <- probe(.x, .p)
   out <- list_along(.x)
@@ -129,6 +130,7 @@ future_map_if <- function(.x, .p, .f, ..., .progress = FALSE, .options = future_
 #' @rdname future_map
 #' @export
 #' @importFrom purrr list_along set_names
+#' @inheritParams purrr::map_at
 future_map_at <- function(.x, .at, .f, ..., .progress = FALSE, .options = future_options()) {
   sel <- inv_which(.x, .at)
   out <- list_along(.x)
