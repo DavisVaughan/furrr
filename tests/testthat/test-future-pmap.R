d2de8603c38dafb52524fc923eef13c6d17e1021
DavisVaughan <davis@rstudio.com>
2020-07-29 16:50:39 -0400

    Revamp `future_pmap()` tests

--- tests/testthat/test-future-pmap.R
+++ tests/testthat/test-future-pmap.R
@@ -1,118 +1,171 @@
 # ------------------------------------------------------------------------------
-# Setup
+# pmap()
 
-.th <- retrieve_test_helpers()
-test_msg <- .th$test_msg
-test_dat <- .th$test_dat
+furrr_test_that("future_pmap() matches pmap() for simple cases", {
+  expect_identical(
+    future_pmap(list(1:3, 4:6, 7:9), ~.x + .y + ..3),
+    pmap(list(1:3, 4:6, 7:9), ~.x + .y + ..3)
+  )
+})
 
-test_l <- list(test_dat, test_dat)
+furrr_test_that("names of `.x` are retained", {
+  skip("Until #116 is fixed")
+
+  x <- c(a = 1, b = 2)
+  y <- c(c = 1, d = 2)
+  expect_named(future_pmap(list(x, y), ~1), c("a", "b"))
+})
 
 # ------------------------------------------------------------------------------
-# Testing
+# atomic variants
 
-for(.e in .th$executors) {
+furrr_test_that("future_pmap_dbl() works", {
+  x <- c(1, 2, 3)
+  y <- c(4, 5, 6)
 
-  # Don't test multicore on non-Mac
-  if(.e == "multicore" && .th$system.os != "Darwin") {
-    next
+  expect_identical(
+    future_pmap_dbl(list(x, y), ~.x + .y),
+    pmap_dbl(list(x, y), ~.x + .y)
+  )
+})
+
+furrr_test_that("future_pmap_int() works", {
+  x <- c(1L, 2L, 3L)
+  y <- c(4L, 5L, 6L)
+
+  expect_identical(
+    future_pmap_int(list(x, y), ~.x + .y),
+    pmap_int(list(x, y), ~.x + .y)
+  )
+})
+
+furrr_test_that("future_pmap_lgl() works", {
+  x <- c(TRUE, FALSE, TRUE)
+  y <- c(FALSE, TRUE, TRUE)
+
+  expect_identical(
+    future_pmap_lgl(list(x, y), ~.x || .y),
+    pmap_lgl(list(x, y), ~.x || .y)
+  )
+})
+
+furrr_test_that("future_pmap_chr() works", {
+  x <- c("a", "b", "c")
+  y <- c("d", "e", "f")
+
+  expect_identical(
+    future_pmap_chr(list(x, y), ~.y),
+    pmap_chr(list(x, y), ~.y)
+  )
+})
+
+furrr_test_that("names of `.x` are retained", {
+  skip("Until #116 is fixed")
+
+  x <- c(a = 1, b = 2)
+  y <- c(c = 1, d = 2)
+  expect_named(future_pmap_dbl(list(x, y), ~1), c("a", "b"))
+})
+
+# ------------------------------------------------------------------------------
+# data frame variants
+
+furrr_test_that("future_pmap_dfr() works", {
+  x <- c("a", "b", "c")
+  y <- c("d", "e", "f")
+
+  expect_identical(
+    future_pmap_dfr(list(x, y), ~data.frame(x = .x, y = .y)),
+    pmap_dfr(list(x, y), ~data.frame(x = .x, y = .y))
+  )
+})
+
+furrr_test_that("future_pmap_dfc() works", {
+  x <- c("a", "b", "c")
+  y <- c("d", "e", "f")
+
+  expect_identical(
+    future_pmap_dfc(list(x, y), ~as.data.frame(set_names(list(.x), .y))),
+    pmap_dfc(list(x, y), ~as.data.frame(set_names(list(.x), .y)))
+  )
+})
+
+# ------------------------------------------------------------------------------
+# Miscellaneous
+
+furrr_test_that("named arguments can be passed through", {
+  vec_mean <- function(.x, .y, na.rm = FALSE) {
+    mean(c(.x,.y), na.rm = na.rm)
   }
 
-  plan(.e, substitute = FALSE)
+  x <- list(c(NA, 1), 1:2)
 
-  test_that(test_msg(.e, "equivalence with pmap()"), {
-    .f <- identical
-    .purrr <- purrr::pmap(test_l, sum)
-    .furrr <- future_pmap(test_l, sum)
-    expect_equal(.purrr, .furrr)
-  })
+  expect_identical(
+    future_pmap(x, vec_mean, na.rm = TRUE),
+    list(1, 1.5)
+  )
+})
 
-  test_that(test_msg(.e, "equivalence with vector pmap()s"), {
-    .f <- ~as.character(sum(.x, .y))
-    .purrr <- purrr::pmap_chr(test_l, .f)
-    .furrr <- future_pmap_chr(test_l, .f)
-    expect_equal(.purrr, .furrr)
-  })
+furrr_test_that("arguments can be matched by name", {
+  x <- list(x = c(1, 2), y = c(3, 5))
 
-  test_that(test_msg(.e, "equivalence with df pmap()s"), {
-    .f <- ~data.frame(x = sum(.x, .y))
-    .purrr <- purrr::pmap_dfr(test_l, .f)
-    .furrr <- future_pmap_dfr(test_l, .f)
-    expect_equal(.purrr, .furrr)
-  })
+  fn <- function(y, x) {y - x}
 
-  test_that(test_msg(.e, "named arguments can be passed through"), {
+  expect_identical(future_pmap_dbl(x, fn), c(2, 3))
+})
 
-    vec_mean <- function(.x, .y, na.rm = FALSE) {
-      mean(c(.x,.y), na.rm = na.rm)
+furrr_test_that("unused components can be absorbed", {
+  x <- list(c(1, 2), c(3, 5))
+
+  fn1 <- function(x) {x}
+  fn2 <- function(x, ...) {x}
+
+  expect_error(future_pmap_dbl(x, fn1))
+  expect_identical(future_pmap_dbl(x, fn2), c(1, 2))
+})
+
+furrr_test_that("globals in `.x` and `.y` are found (#16)", {
+  fn1 <- function(x) sum(x, na.rm = TRUE)
+  fn2 <- function(x) sum(x, na.rm = FALSE)
+
+  x <- list(c(1, 2, NA), c(2, 3, 4))
+
+  fns1 <- map(x, ~ purrr::partial(fn1, x = .x))
+  fns2 <- map(x, ~ purrr::partial(fn2, x = .x))
+
+  expect_identical(
+    future_pmap(list(fns1, fns2), ~c(.x(), .y())),
+    list(c(3, NA), c(9, 9))
+  )
+})
+
+test_that("globals in `.l` are only exported to workers that use them", {
+  local_multisession()
+
+  # Use `local()` to ensure that the wrapper functions and the anonymous
+  # functions created with `~` don't pick up extra globals
+  my_wrapper1 <- local({
+    my_mean1 <- function(x) mean(x, na.rm = TRUE)
+
+    function(x) {
+      my_mean1(x)
+      exists("my_mean1")
     }
-
-    test_l_na <- test_l
-    test_l_na[[1]][1] <- NA
-
-    .purrr <- purrr::pmap(test_l_na, vec_mean, na.rm = TRUE)
-    .furrr <- future_pmap(test_l_na, vec_mean, na.rm = TRUE)
-
-    expect_equal(.purrr, .furrr)
   })
 
-  test_that(test_msg(.e, "arguments can be matched by name"), {
+  my_wrapper2 <- local({
+    my_mean2 <- function(x) mean(x, na.rm = FALSE)
 
-    test_l_named <- test_l
-    names(test_l_named) <- c("x", "y")
-    test_l_named[["y"]] <- test_l_named[["y"]] * 2
-
-    .f <- function(y, x) {y - x}
-
-    .purrr <- purrr::pmap(test_l_named, .f)
-    .furrr <- future_pmap(test_l_named, .f)
-
-    expect_equal(.purrr, .furrr)
+    function(x) {
+      my_mean2(x)
+      exists("my_mean1")
+    }
   })
 
-  test_that(test_msg(.e, "unused components can be absorbed"), {
+  x <- list(my_wrapper1, my_wrapper2)
 
-    .f_bad <- function(x)      {x}
-    .f     <- function(x, ...) {x}
-
-    .purrr <- purrr::pmap(test_l, .f)
-    .furrr <- future_pmap(test_l, .f)
-
-    expect_error(future_pmap(test_l, .f_bad))
-    expect_equal(.purrr, .furrr)
-  })
-
-  # See issue #16
-  test_that(test_msg(.e, "Globals in .l are found (.l could be a fn)"), {
-
-    my_robust_sum <- function(x) sum(x, na.rm = TRUE)  # Can you find me?
-    my_robust_sum2 <- function(x) sum(x, na.rm = TRUE) # Can you find me?
-    multi_x <- list(c(1, 2, NA), c(2, 3, 4))
-    Xs <- purrr::map(multi_x, ~ purrr::partial(my_robust_sum, .x))
-    Ys <- purrr::map(multi_x, ~ purrr::partial(my_robust_sum2, .x))
-
-    .purrr <- purrr::pmap(.l = list(Xs, Ys), .f = ~c(.x(), .y()))
-    .furrr <- future_pmap(.l = list(Xs, Ys), .f = ~c(.x(), .y()))
-
-    expect_equal(.purrr, .furrr)
-  })
-
-  test_that(test_msg(.e, "Globals in .l are only exported to workers that use them"), {
-
-    # This test is difficult to check without manually inspecting what gets exported
-
-    my_mean  <- function(x) { mean(x, na.rm = TRUE) } # Exported to worker 1
-    my_mean2 <- function(x) { mean(x, na.rm = TRUE) } # Exported to worker 2
-
-    my_wrapper  <- function(x) { my_mean(x)  }
-    my_wrapper2 <- function(x) { my_mean2(x) }
-
-    .l <- list(my_wrapper, my_wrapper2)
-    .l2 <- list(1, 2)
-
-    .purrr <- future_pmap(list(.l, .l2), ~.x(.y))
-    .furrr <- purrr::pmap(list(.l, .l2), ~.x(.y))
-
-    expect_equal(.purrr, .furrr)
-  })
-
-}
+  expect_identical(
+    future_pmap_lgl(list(x), .f = ~.x(c(1, NA))),
+    c(TRUE, FALSE)
+  )
+})
