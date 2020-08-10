c6fa9bccdcebd4488675f3b12cc2ccade913e09e
DavisVaughan <davis@rstudio.com>
2020-08-10 13:12:39 -0400

    Update `future_modify_if()` to purrr 0.3.0 semantics

--- R/future-modify.R
+++ R/future-modify.R
@@ -202,6 +202,7 @@ future_modify_if <- function(.x,
                              .p,
                              .f,
                              ...,
+                             .else = NULL,
                              .options = furrr_options(),
                              .env_globals = parent.frame(),
                              .progress = deprecated()) {
@@ -214,10 +215,165 @@ future_modify_if.default <- function(.x,
                                      .p,
                                      .f,
                                      ...,
+                                     .else = NULL,
                                      .options = furrr_options(),
                                      .env_globals = parent.frame(),
                                      .progress = deprecated()) {
   sel <- probe(.x, .p)
-  .x[sel] <- future_map(.x[sel], .f, ..., .options = .options, .env_globals = .env_globals)
+  index <- seq_along(.x)
+
+  result <- future_map(
+    .x = .x[sel],
+    .f = .f,
+    ...,
+    .options = .options,
+    .env_globals = .env_globals
+  )
+
+  locs <- index[sel]
+
+  for (i in seq_along(locs)) {
+    loc <- locs[[i]]
+    .x[[loc]] <- result[[i]]
+  }
+
+  if (!is_null(.else)) {
+    result_else <- future_map(
+      .x = .x[!sel],
+      .f = .else,
+      ...,
+      .options = .options,
+      .env_globals = .env_globals
+    )
+
+    locs_else <- index[!sel]
+
+    for (i in seq_along(locs_else)) {
+      loc <- locs_else[[i]]
+      .x[[loc]] <- result_else[[i]]
+    }
+  }
+
+  .x
+}
+
+#' @export
+future_modify_if.integer <- function(.x,
+                                     .p,
+                                     .f,
+                                     ...,
+                                     .else = NULL,
+                                     .options = furrr_options(),
+                                     .env_globals = parent.frame(),
+                                     .progress = deprecated()) {
+  future_modify_if_variant(
+    .x = .x,
+    .p = .p,
+    .f = .f,
+    ...,
+    .else = .else,
+    .options = .options,
+    .env_globals = .env_globals,
+    .future_map_variant = future_map_int
+  )
+}
+
+#' @export
+future_modify_if.double <- function(.x,
+                                    .p,
+                                    .f,
+                                    ...,
+                                    .else = NULL,
+                                    .options = furrr_options(),
+                                    .env_globals = parent.frame(),
+                                    .progress = deprecated()) {
+  future_modify_if_variant(
+    .x = .x,
+    .p = .p,
+    .f = .f,
+    ...,
+    .else = .else,
+    .options = .options,
+    .env_globals = .env_globals,
+    .future_map_variant = future_map_dbl
+  )
+}
+
+#' @export
+future_modify_if.character <- function(.x,
+                                       .p,
+                                       .f,
+                                       ...,
+                                       .else = NULL,
+                                       .options = furrr_options(),
+                                       .env_globals = parent.frame(),
+                                       .progress = deprecated()) {
+  future_modify_if_variant(
+    .x = .x,
+    .p = .p,
+    .f = .f,
+    ...,
+    .else = .else,
+    .options = .options,
+    .env_globals = .env_globals,
+    .future_map_variant = future_map_chr
+  )
+}
+
+#' @export
+future_modify_if.logical <- function(.x,
+                                       .p,
+                                       .f,
+                                       ...,
+                                       .else = NULL,
+                                       .options = furrr_options(),
+                                       .env_globals = parent.frame(),
+                                       .progress = deprecated()) {
+  future_modify_if_variant(
+    .x = .x,
+    .p = .p,
+    .f = .f,
+    ...,
+    .else = .else,
+    .options = .options,
+    .env_globals = .env_globals,
+    .future_map_variant = future_map_lgl
+  )
+}
+
+future_modify_if_variant <- function(.x,
+                                     .p,
+                                     .f,
+                                     ...,
+                                     .else,
+                                     .options,
+                                     .env_globals,
+                                     .future_map_variant) {
+  sel <- probe(.x, .p)
+
+  result <- .future_map_variant(
+    .x = .x[sel],
+    .f = .f,
+    ...,
+    .options = .options,
+    .env_globals = .env_globals
+  )
+
+  .x[sel] <- result
+
+  if (!is_null(.else)) {
+    not_sel <- !sel
+
+    result <- .future_map_variant(
+      .x = .x[not_sel],
+      .f = .else,
+      ...,
+      .options = .options,
+      .env_globals = .env_globals
+    )
+
+    .x[not_sel] <- result
+  }
+
   .x
 }
--- tests/testthat/test-future-modify.R
+++ tests/testthat/test-future-modify.R
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------------------------
+# future_modify()
+
 furrr_test_that("future_modify() default method works", {
   expect_identical(future_modify(list(1, 2), ~3), list(3, 3))
   expect_identical(future_modify(data.frame(x = 1, y = 2), ~3), data.frame(x = 3, y = 3))
@@ -22,4 +25,23 @@ furrr_test_that("future_modify() variants work", {
 furrr_test_that("future_modify(<pairlist>) works", {
   x <- as.pairlist(list(1, 2))
   expect_identical(class(future_modify(x, ~.x)), "pairlist")
+})
+
+# ------------------------------------------------------------------------------
+# future_modify_if()
+
+furrr_test_that("future_modify_if() default works", {
+  expect_identical(future_modify_if(list(1, 2), ~.x == 1, ~3), list(3, 2))
+  expect_identical(future_modify_if(data.frame(x = 1, y = 2), ~.x == 1, ~3), data.frame(x = 3, y = 2))
+})
+
+furrr_test_that("future_modify_if() `.else` works for default", {
+  expect_identical(future_modify_if(list(1, 2, 1, 4), ~.x == 1, ~3, .else = ~4), list(3, 4, 3, 4))
+})
+
+furrr_test_that("future_modify_if() variants works", {
+  expect_identical(future_modify_if(c(1L, 2L, 3L), ~.x == 1L, ~2L, .else = ~3L), c(2L, 3L, 3L))
+  expect_identical(future_modify_if(c(1, 2, 3), ~.x == 1, ~2, .else = ~3), c(2, 3, 3))
+  expect_identical(future_modify_if(c("a", "b", "c"), ~.x == "a", toupper, .else = ~"d"), c("A", "d", "d"))
+  expect_identical(future_modify_if(c(TRUE, FALSE, TRUE), ~.x == TRUE, ~TRUE, .else = ~NA), c(TRUE, NA, TRUE))
 })
